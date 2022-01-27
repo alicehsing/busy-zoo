31712d4b9977325b2c3bf6ca054dd3db9d3093e2
Alice Hsing <alicehsing@gmail.com>
2022-01-26 18:05:58 -0800

    set up first section of jsx on App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="fight">
+          <p>🐼</p>
+          <button>Panda attacks Hedgehog</button>
+          <button>Panda gets stronger</button>
+          <p>🦔 </p>
+          <button>Hedgehog attacks Panda</button>
+          <button>Hedgehog gets stronger</button>
+        </section>
+
+        <section className="door">
+
+        </section>
+
+        <section className="parade">
+
+        </section>
       </header>
     </div>
   );
