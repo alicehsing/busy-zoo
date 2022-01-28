2f8ad736f6eb2e26f9d07fd6aedb08eab5416fab
Alice Hsing <alicehsing@gmail.com>
2022-01-27 17:44:36 -0800

    got animal buttons for grow and shrink to work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [pandaSize, setPandaSize] = useState(30);
+  const [hedgehogSize, sethedgehogSize] = useState(30);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <section className="fight">
-          <p>🐼</p>
-          <button>Panda attacks Hedgehog</button>
-          <button>Panda gets stronger</button>
-          <p>🦔 </p>
-          <button>Hedgehog attacks Panda</button>
-          <button>Hedgehog gets stronger</button>
-        </section>
+      <section className="fight">
+        <div className="panda">
+          {/* the width of the panda should be 2 times whatever the hedgehog size is in state */}
+          <img src="panda.png" width={pandaSize * 1.5} />
+          <button onClick={() => setPandaSize(pandaSize + 5)}>Panda gets stronger</button>
+          <button onClick={() => sethedgehogSize(hedgehogSize - 5)}>Panda attacks Hedgehog</button>
+        </div>
+        <div className="hedgehog">
+          <img src="hedgehog.png" width={hedgehogSize * 1} />
+          <button onClick={() => sethedgehogSize(hedgehogSize + 5)}>Hedgehog gets stronger</button>
+          <button onClick={() => setPandaSize(pandaSize - 5)}>Hedgehog attacks Panda</button>
+        </div>
+      </section>
 
-        <section className="door">
+      <section className="door">
 
-        </section>
+      </section>
 
-        <section className="parade">
+      <section className="parade">
 
-        </section>
-      </header>
+      </section>
+     
     </div>
   );
 }
