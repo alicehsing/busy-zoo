9c09ac54815b9eea5fe14ba797a4fafcdd01cfe1
Alice Hsing <alicehsing@gmail.com>
2022-02-17 16:53:56 -0800

    wrote third test

--- src/Sign.test.js
+++ src/Sign.test.js
@@ -18,3 +18,16 @@ test('render a kangaroo emoji if you click +KANGAROO button', () => {
   fireEvent.click(buttonKangarooEl);
   expect(animalEl.textContent).toBe('🦘');
 });
+
+test('renders an bigger size panda on click of a button', () => {
+
+  const { rerender } = render(<App />);
+  const imgEl = screen.getByAltText('panda');
+  const pandaButton = screen.getByText('Panda gets stronger');
+  
+  fireEvent.click(pandaButton);
+  rerender(<App />);
+  console.log(imgEl);
+  expect(imgEl.style.width).toBe('52.5px');
+
+});
